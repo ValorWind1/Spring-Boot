656101a63a0019b7d3c70f9641d9a9a254f5192e
ValorWind1 <skyjdani@gmail.com>
2019-10-06 14:05:07 -0400

    spring api working with postgres db

--- demo/src/main/java/com/example/demo/api/PersonController.java
+++ demo/src/main/java/com/example/demo/api/PersonController.java
@@ -3,6 +3,7 @@ package com.example.demo.api;
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -24,7 +25,7 @@ public class PersonController {
     }
 
     @PostMapping
-    public void addPerson (@RequestBody Person person){
+    public void addPerson (@Valid @NonNull @RequestBody Person person){
         personService.addPerson(person);
     }
 
